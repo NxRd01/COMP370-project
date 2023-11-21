b36847980264b7f1fe379e14b4bfd35955e98ced
William Liu Man Hin <69032106+NxRd01@users.noreply.github.com>
2023-11-20 19:21:57 -0500

    -collect articles from all sources -collect articles day by day

--- scripts/collect_articles.py
+++ scripts/collect_articles.py
@@ -1,9 +1,42 @@
 from newsapi import NewsApiClient
 import json     
+from pathlib import Path
+from datetime import datetime,timedelta
+from time import sleep as wait
 
 
 def main(): 
-    pass
+    with open(Path(__file__).parent.parent / 'data' / 'apikey.txt','r') as f:
+        apikey = f.read()
+        apikey = apikey.strip()
+        
+    with open(Path(__file__).parent.parent / 'data' / 'sources_id.txt','r') as f:
+        sources = f.read()
+        sources = sources.strip()
+         
+    #Initialise newsapi with the api key 
+    newsapi = NewsApiClient(api_key=apikey)
+    
+    #Get the articles from the sources for each day 
+    current_date = datetime(2023,10,20)
+    day_after = current_date + timedelta(days=1)
+    articles_list = []
+    
+    while current_date < datetime(2023,11,19):
+        articles = newsapi.get_everything(q="Taylor Swift",
+                                          sources=sources,
+                                          language='en',
+                                          sort_by='relevancy',
+                                          from_param=current_date,
+                                          to=day_after,
+                                          page_size=100)
+        articles_list.append(articles)
+        current_date = day_after
+        day_after = current_date + timedelta(days=1)
+        wait(1)
 
+    with open(Path(__file__).parent.parent / 'data' / 'articles.json','w') as f:
+        json.dump(articles_list,f)
+        
 if __name__ == '__main__':
     main()
--- scripts/collect_sources.py
+++ scripts/collect_sources.py
@@ -6,7 +6,7 @@ import argparse
 def main():
     
     args = argparse.ArgumentParser()
-    args.add_argument('--keyword',type=str,default='entertainment')
+    args.add_argument('-k','--keyword',type=str,default='entertainment')
     args = args.parse_args()
     
     keyword = args.keyword.lower()
@@ -21,10 +21,11 @@ def main():
     #keep only the sources that are from the us or canada
     sources = [source for source in sources['sources'] if source['country'] == 'us' or source['country'] == 'ca']
     #Keep only the sources that have 'entertainment' in their description
-    sources = [source for source in sources if keyword in source['description'].lower()]
+    # sources = [source for source in sources if keyword in source['description'].lower()]
     
-    with open(Path(__file__).parent.parent / 'data' / 'sources.json','w') as f:
-        json.dump(sources,f,indent=4)
+    sources_name_list = [source['id'] for source in sources]
+    with open(Path(__file__).parent.parent / 'data' / 'sources_id.txt','w') as f:
+        f.write(','.join(sources_name_list))
     
     
 
